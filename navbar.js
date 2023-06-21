0bda8860f833a5eae5f8aac209814be98b63e968
Assemie <assemie02@gmail.com>
2023-06-21 19:08:02 +0200

    Improved Mobile Nav Bar

--- navbar.js
+++ navbar.js
@@ -1,9 +1,12 @@
 function openNav() {
     document.getElementById("mySidepanel").style.width = "200px";
     console.log("GEH AUF")
+    document.getElementById("openbtn").style.visibility = "hidden";
   }
   
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0px";
     console.log("GEH ZU")
+    document.getElementById("openbtn").style.visibility = "visible";
+
   }
